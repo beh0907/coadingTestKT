5dee86db966bf1bd7ea66242d1b017ffe63c591d
beh0907 <qkrdlsguq@naver.com>
2024-03-25 17:25:34 +0900

    백준 - 13901 로봇 - 14620 꽃길

---
+++ src/main/kotlin/Backjoon13901.kt
@@ -0,0 +1,52 @@
+//로봇
+//입력값들의 문제로 trim 추가
+//https://www.acmicpc.net/problem/13901
+fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
+    //지도 사이즈를 얻어온다
+    val (w, h) = readln().trim().split(" ").map { it.toInt() }
+
+    //방해물 수
+    val hurdles = readln().trim().toInt()
+
+    //맵
+    val map = Array(w) { BooleanArray(h) }
+
+    repeat(hurdles) {
+        //방해물의 위치를 가져온다
+        val (x, y) = readln().trim().split(" ").map { it.toInt() }
+
+        //방해물 위치에 값 설정
+        map[x][y] = true
+    }
+
+    //로봇의 시작 위치 설정
+    var (startX, startY) = readln().trim().split(" ").map { it.toInt() }
+    map[startX][startY] = true
+
+    //로봇을 이동 시킬 4개의 고정 명령
+    val commands = readln().trim().split(" ").map { it.toInt() - 1 }
+    val dx = arrayOf(-1, 1, 0, 0)
+    val dy = arrayOf(0, 0, -1, 1)
+
+    var command = 0
+    var count = 0
+    while (count < 4) {
+        val moveX = startX + dx[commands[command]]
+        val moveY = startY + dy[commands[command]]
+
+        //이동할 위치가 맵 내부이며, 방문한 이력이 없다면
+        if (moveX in 0..<w && moveY in 0..<h && !map[moveX][moveY]) {
+            map[moveX][moveY] = true
+
+            startX = moveX
+            startY = moveY
+            count = 0
+        } else {
+            count++
+            command = (command + 1) % commands.size
+        }
+
+    }
+
+    println("$startX $startY")
+}
---
+++ src/main/kotlin/Backjoon14620.kt
@@ -0,0 +1,95 @@
+import kotlin.math.min
+
+//꽃길
+//https://www.acmicpc.net/problem/14620
+
+
+val dx = arrayOf(-1, 0, 1, 0)
+val dy = arrayOf(0, 1, 0, -1)
+var result = Int.MAX_VALUE
+lateinit var map: Array<IntArray> // 맵
+lateinit var dp: Array<IntArray> // 씨앗 발아 비용
+lateinit var visited: Array<BooleanArray> // 방문
+
+fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
+    val size = readln().toInt()
+    map = Array(size) { IntArray(size) }
+    dp = Array(size) { IntArray(size) { Int.MAX_VALUE } }
+    visited = Array(size) { BooleanArray(size) }
+
+    //맵 비용 할당
+    repeat(size) { i ->
+        map[i] = readln().split(" ").map { it.toInt() }.toIntArray()
+    }
+
+    //위치별 만개 비용
+    for (i in 1..<size - 1) {
+        for (j in 1..<size - 1) {
+            var cost = map[i][j]
+
+            repeat(4) {
+                val moveX = i + dx[it]
+                val moveY = j + dy[it]
+                cost += map[moveX][moveY]
+            }
+
+            dp[i][j] = cost
+        }
+    }
+
+    dfs(0, 0)
+
+    println(result)
+}
+
+fun dfs(cost: Int, count: Int) {
+    //꽃을 3개 다 심었다면 최소값을 저장하고 종료
+    if (count == 3) {
+        result = min(result, cost)
+        return
+    }
+
+    //반복 처리
+    //테두리 부분은 꽃이 피어도 맵 밖으로 나가기 때문에 제외
+    for (i in 1..<map.size - 1) {
+        for (j in 1..<map.size - 1) {
+            //주변에 이미 꽃이 피어있다면 취소한다
+            if (!isPossible(i, j)) continue
+
+            //방문처리
+            checkVisited(i, j, true)
+
+            //동작 수행
+            dfs(cost + dp[i][j], count + 1)
+
+            //방문처리 취소
+            checkVisited(i, j, false)
+        }
+    }
+}
+
+//씨앗을 심을 수 있는지 확인한다
+fun isPossible(x: Int, y: Int): Boolean {
+    repeat(4) { dir ->
+        val moveX = x + dx[dir]
+        val moveY = y + dy[dir]
+
+        if (visited[moveX][moveY]) return false
+    }
+
+    return true
+}
+
+//꽃이 핀 영역을 설정한다
+fun checkVisited(x: Int, y: Int, value: Boolean) {
+    //현재 위치 체크
+    visited[x][y] = value
+
+    //주변 영역 체크
+    repeat(4) { dir ->
+        val moveX = x + dx[dir]
+        val moveY = y + dy[dir]
+
+        visited[moveX][moveY] = value
+    }
+}
