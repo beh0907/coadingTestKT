b6a4a2e1d952a4da975807895e46052fd0c0eda9
beh0907 <qkrdlsguq@naver.com>
2024-04-05 17:05:19 +0900

    백준 - 5558 Cheese - 6087 레이저 통신 (메모리 초과)

---
+++ src/main/kotlin/Backjoon5558.kt
@@ -0,0 +1,104 @@
+import java.util.*
+
+//Cheese
+//https://www.acmicpc.net/problem/5558
+
+private val dirs = arrayOf(Pair(-1, 0), Pair(0, 1), Pair(1, 0), Pair(0, -1))
+
+private lateinit var map: Array<IntArray>
+
+fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
+    val (x, y, cheeses) = readln().trim().split(" ").map { it.toInt() }
+    map = Array(x) { IntArray(y) }
+
+    var (startX, startY) = Pair(0, 0)
+
+    repeat(x) { i ->
+
+        readln().trim().toCharArray().onEachIndexed { j, ch ->
+
+            when (ch) {
+                'S' -> { //시작 지점
+                    startX = i
+                    startY = j
+
+                    //시작 지점도 마찬가지로 빈공간 설정
+                    map[i][j] = 0
+                }
+
+                '.' -> { // 빈공간
+                    map[i][j] = 0
+                }
+
+                'X' -> { // 장애물
+                    map[i][j] = -1
+                }
+
+                else -> { //치즈
+                    map[i][j] = ch - '0'
+                }
+            }
+        }
+    }
+
+    println(bfs(startX, startY, map, cheeses))
+}
+
+private fun bfs(startX: Int, startY: Int, map: Array<IntArray>, cheeses: Int): Int {
+    //시작지점 방문 처리
+    var visited = Array(map.size) { BooleanArray(map[0].size) }
+    visited[startX][startY]
+
+    //큐 삽입
+    val queue: Queue<Rat> = LinkedList()
+    queue.offer(Rat(startX, startY, 1, 0))
+
+    while (queue.isNotEmpty()) {
+        val rat = queue.poll()
+
+        val hp = rat.hp
+
+        //현재 체력이 치즈 개수보다 많다면 다 먹었다는 의미이므로 결과 리턴
+        if (hp > cheeses) return rat.move
+
+        for (dir in dirs) {
+            val dx = rat.x + dir.first
+            val dy = rat.y + dir.second
+
+            //맵 밖 위치라면 continue
+            if (dx !in map.indices || dy !in map[0].indices) continue
+
+            //장애물이라면 continue
+            if (map[dx][dy] == -1) continue
+
+            //방문한 이력이 있다면 continue
+            if (visited[dx][dy]) continue
+
+            //이동할 칸이 치즈이며, 먹을 수 있는 상태라면
+            if (map[dx][dy] in 1..hp) {
+                //먹었다면 해당 위치에서 재시작하므로 큐와 방문 값을 초기화한다
+                queue.clear()
+                queue.offer(Rat(dx, dy, hp + 1, rat.move + 1))
+                visited = Array(map.size) { BooleanArray(map[0].size) }
+
+                //치즈를 먹었기 때문에 삭제처리한다
+                map[dx][dy] = 0
+
+                //새로 시작하기 때문에 방향 배열을 종료시킨다
+                break
+            } else { //빈 공간이거나 먹지 못하는 치즈라면
+                //방문 처리
+                visited[dx][dy] = true
+
+                //이동 큐 추가
+                queue.offer(Rat(dx,dy,hp, rat.move + 1))
+            }
+
+        }
+    }
+
+    return 0
+}
+
+
+data class Rat(val x: Int, val y: Int, val hp: Int, val move: Int)
---
+++ src/main/kotlin/Backjoon6087.kt
@@ -0,0 +1,81 @@
+import java.util.*
+import kotlin.math.abs
+
+//레이저 통신
+//https://www.acmicpc.net/problem/6087
+private val dirs = arrayOf(Pair(-1, 0), Pair(0, 1), Pair(1, 0), Pair(0, -1))
+
+fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
+    val (y, x) = readln().trim().split(" ").map { it.toInt() }
+    val map = Array(x) { CharArray(y) }
+    val targets = ArrayList<Node>()
+
+    repeat(x) { i ->
+        map[i] = readln().toCharArray()
+
+        map[i].forEachIndexed { j, ch ->
+            // 2개의 C중 아무거나 사용해도 상관없다
+            if (ch == 'C') {
+                targets.add(Node(i, j, -5, -1))
+            }
+        }
+    }
+
+    println(bfs(targets[0], targets[1], map))
+}
+
+private fun bfs(start: Node, goal: Node, map: Array<CharArray>): Int {
+    val queue: PriorityQueue<Node> = PriorityQueue(compareBy { it.move }) // 우선순위 큐 선언
+
+    // 방문 정보 초기화
+    val visited = Array(map.size) { IntArray(map[0].size) { Int.MAX_VALUE } }
+
+    // 시작지점 4방향을 큐에 삽입한다
+    for (i in dirs.indices) queue.add(Node(start.x, start.y, i, 0))
+
+    while (queue.isNotEmpty()) {
+        val (x, y, dir, move) = queue.poll()
+
+        //더 적은 값의 배치 정보가 있다면 continue
+        if (move > visited[x][y]) continue
+
+        // 목표를 찾았다면 결과 정보를 리턴한다
+        if (x == goal.x && y == goal.y) {
+            return move
+        }
+
+        for (i in dirs.indices) {
+            val dx = x + dirs[i].first
+            val dy = y + dirs[i].second
+
+            // 맵 밖 위치라면 continue
+            if (dx !in map.indices || dy !in map[0].indices) continue
+
+            // 장애물이라면 continue
+            if (map[dx][dy] == '*') continue
+
+            //종/횡 동일한 라인이라면 continue
+            if (abs(dir - i) == 2) continue
+
+            // 방향을 꺾어야 한다면 1 증가
+            val nextMirror = if (dir == i) move else move + 1
+
+            //더 많은 거울을 배치하게 된다면 continue
+            if (nextMirror > visited[dx][dy])
+
+            //방문 값 추가
+            visited[dx][dy] = nextMirror
+
+
+            queue.offer(Node(dx,dy, i, nextMirror))
+        }
+    }
+
+    return -1
+}
+
+data class Node(val x: Int, val y: Int, val dir: Int, val move: Int) : Comparable<Node> {
+    override fun compareTo(other: Node): Int {
+        return this.move - other.move
+    }
+}
